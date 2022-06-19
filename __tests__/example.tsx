4391cbda692abe7b437050fdf9d44a877b6b515d
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-06-19 19:23:58 +0100

    🧪 test setup

---
+++ __tests__/example.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { Text, View } from "react-native";
+
+const Greeting = ({ name }) => {
+  return (
+    <View>
+      <Text>Hello {name}!</Text>
+    </View>
+  );
+};
+
+describe("Greeting", () => {
+  it("renders a greeting message based on the name prop", () => {
+    const { debug, getByText } = render(<Greeting name="Kalle" />);
+
+    debug();
+
+    expect(getByText("Hello Kalle!")).toBeDefined();
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.(js)$": "<rootDir>/node_modules/babel-jest",
+    // "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  testPathIgnorePatterns: ["\\.snap$", "<rootDir>/node_modules/"],
+  cacheDirectory: ".jest/cache",
+};
